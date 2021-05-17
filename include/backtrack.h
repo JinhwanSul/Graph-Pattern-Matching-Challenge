5a16f902010bbf28e341e1c8284727caf46784a2
JinhwanSul <jhsul516@gmail.com>
2021-05-17 21:01:23 +0900

    skeleton Backtrack

--- include/backtrack.h
+++ include/backtrack.h
@@ -17,6 +17,20 @@ class Backtrack {
 
   void PrintAllMatches(const Graph &data, const Graph &query,
                        const CandidateSet &cs);
+
+ private:
+	//TODO: JinHwan
+	size_t NextU(size_t current_u);
+	bool EmbeddingCondition(const Graph &data, const Graph &query, std::pair<size_t, size_t> u_v);
+	//TODO: JaeHyung
+	size_t SelectRoot(const Graph &query, const CandidateSet &cs);
+	bool PushU(size_t u); // true: v-stack is not empty
+	void PrintnClear();
+
+	size_t GoBack(size_t current_state);
+	std::vector<std::pair<size_t, std::stack<size_t>* > > state_space;
+	std::vector<std::pair<size_t, size_t> > partial_embedding;
+   
 };
 
 #endif  // BACKTRACK_H_
--- src/backtrack.cc
+++ src/backtrack.cc
@@ -5,7 +5,10 @@
 
 #include "backtrack.h"
 
-Backtrack::Backtrack() {}
+Backtrack::Backtrack() {
+  // TODO: state space initiailize
+  // initialize v-stacks
+}
 Backtrack::~Backtrack() {}
 
 void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
@@ -13,4 +16,42 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
   std::cout << "t " << query.GetNumVertices() << "\n";
 
   // implement your code here.
+}
+
+size_t Backtrack::GoBack(size_t current_state) {
+  while (state_space[current_state].second->empty()) {
+    current_state--;
+    state_space[current_state].second->pop();
+  }
+}
+
+void Backtrack::PrintAllMatches(const Graph &data, const Graph &query, const CandidateSet &cs) {
+  size_t root = SelectRoot(query, cs); 
+  PushU(root);
+  size_t current_state = 0;
+  size_t next_u, next_state;
+  
+  while (state_space[0].second->size() == 0) {
+    
+    if (current_state == query.GetNumVertices()) {
+      PrintnClear();
+      current_state = GoBack(current_state);
+    }
+
+    next_state = state_space[current_state + 1].first;
+    
+    if (next_state == -1){
+      next_u = NextU(state_space[current_state].first);
+      current_state++;
+      if (!PushU(next_u)){
+        current_state = GoBack(current_state);
+      }
+    } else {
+      next_u = next_state;
+      current_state++;
+      if (!PushU(next_u)){
+        current_state = GoBack(current_state);
+      }
+    }    
+  } 
 }
