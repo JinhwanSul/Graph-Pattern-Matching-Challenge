6c12a411a8fe8794b8ca75e08f47ea9381011576
JinhwanSul <jhsul51@snu.ac.kr>
2021-05-24 22:32:25 +0900

    Jump error fixed

--- include/backtrack.h
+++ include/backtrack.h
@@ -27,7 +27,7 @@ class Backtrack {
   bool PushU(size_t u, size_t current_state, const CandidateSet &cs, const Graph &data, const Graph &query); // true: v-stack is not empty
   void PrintnClear(size_t current_state);
   
-  void Jump(size_t current_state);
+  size_t Jump(size_t current_state);
 
   std::vector<std::pair<size_t, std::stack<size_t>>> state_space;
   std::map<size_t, size_t> partial_embedding;
